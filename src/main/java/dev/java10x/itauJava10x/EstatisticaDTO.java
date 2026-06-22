c31559ee9359fa108db8282433126b864a909907
Gabriel Lusa Marostica <gabriellmarostica@gmail.com>
2026-06-22 15:07:54 -0300

    feat: cria o DTO das estatisticas e uma rota post para adicionar uma transacao

---
+++ src/main/java/dev/java10x/itauJava10x/EstatisticaDTO.java
@@ -0,0 +1,20 @@
+package dev.java10x.itauJava10x;
+
+import lombok.Getter;
+
+@Getter
+public class EstatisticaDTO {
+    private final Long count;
+    private final double avg;
+    private final double max;
+    private final double min;
+    private final double sum;
+
+    public EstatisticaDTO(Long count, double avg, double max, double min, double sum) {
+        this.count = count;
+        this.avg = avg;
+        this.max = max;
+        this.min = min;
+        this.sum = sum;
+    }
+}
