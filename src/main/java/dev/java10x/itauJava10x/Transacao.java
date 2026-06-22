b246832b7f97b44ef79c13307192f2a2c3b4da27
Gabriel Lusa Marostica <gabriellmarostica@gmail.com>
2026-06-22 14:39:33 -0300

    feat: cria a classe basica transacao

---
+++ src/main/java/dev/java10x/itauJava10x/Transacao.java
@@ -0,0 +1,18 @@
+package dev.java10x.itauJava10x;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transacao {
+
+    private BigDecimal valor;
+    private OffsetDateTime dataHora;
+
+}
