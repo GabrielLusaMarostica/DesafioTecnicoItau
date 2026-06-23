c787cc2224b3c0d5213229d9e5c84116b85d3147
Gabriel Lusa Marostica <gabriellmarostica@gmail.com>
2026-06-23 15:42:38 -0300

    feat: adiciona o repository em memoria e finaliza as validacoes para adicionar uma transacao

---
+++ src/main/java/dev/java10x/itauJava10x/TransacaoRepository.java
@@ -0,0 +1,29 @@
+package dev.java10x.itauJava10x;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class TransacaoRepository {
+
+    List<TransacaoRequest> listaDeTransacoes = new ArrayList<>();
+
+    //salvar os dados em uma lista
+    public void salvarDados(TransacaoRequest transacaoRequest){
+        listaDeTransacoes.add(transacaoRequest);
+    }
+
+    // apagar essa lista depois de 60 segundos
+    public void limparDados(TransacaoRequest transacaoRequest){
+
+
+    }
+
+    //apagar todas as transacoes da lista
+    public void deletarDados(){
+        listaDeTransacoes.clear();
+    }
+
+}
--- src/main/java/dev/java10x/itauJava10x/TransacoesController.java
+++ src/main/java/dev/java10x/itauJava10x/TransacoesController.java
@@ -3,10 +3,7 @@ package dev.java10x.itauJava10x;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transacao")
@@ -19,10 +16,15 @@ public class TransacoesController {
     public ResponseEntity adicionar(@RequestBody TransacaoRequest transacaoRequest){
         try{
             transacaoService.validarTransacao(transacaoRequest);
+            transacaoService.salvarDados(transacaoRequest);
             return ResponseEntity.status(HttpStatus.CREATED).build();
 
         } catch(IllegalArgumentException exception){
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
+
+
 }
