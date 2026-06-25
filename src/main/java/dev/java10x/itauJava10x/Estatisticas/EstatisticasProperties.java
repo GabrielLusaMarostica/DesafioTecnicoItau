96210a3c69ab45eafacba72efcf2977511aaf5c3
Gabriel Lusa Marostica <gabriellmarostica@gmail.com>
2026-06-25 17:18:05 -0300

    feat: adiciona validacoes no properties e adiciona dependencias para documentacao

--- src/main/java/dev/java10x/itauJava10x/Estatisticas/EstatisticasProperties.java
+++ src/main/java/dev/java10x/itauJava10x/Estatisticas/EstatisticasProperties.java
@@ -1,7 +1,13 @@
 package dev.java10x.itauJava10x.Estatisticas;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @ConfigurationProperties(prefix = "estatistica")
-public record EstatisticasProperties(Integer segundos) {
+public record EstatisticasProperties(@NotNull(message = "Os segundos não podem ser nulos")
+                                     @Min(value = 1, message = "O tempo deve ser de no mínimo 1 segundo")
+                                     Integer segundos) {
 }
