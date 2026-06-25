2b95a47a44a66b54b4f623e8fbda48cbe5c5280c
Gabriel Lusa Marostica <gabriellmarostica@gmail.com>
2026-06-25 16:32:04 -0300

    feat: adiciona logs nos endpoints

--- src/main/java/dev/java10x/itauJava10x/Transacoes/TransacoesController.java
+++ src/main/java/dev/java10x/itauJava10x/Transacoes/TransacoesController.java
@@ -1,10 +1,12 @@
 package dev.java10x.itauJava10x.Transacoes;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/transacao")
 public class TransacoesController {
@@ -15,19 +17,23 @@ public class TransacoesController {
     @PostMapping
     public ResponseEntity adicionar(@RequestBody TransacaoRequest transacaoRequest){
         try{
+            log.info("Transacao foi adicionado com sucesso");
             transacaoService.validarTransacao(transacaoRequest);
             transacaoService.salvarDados(transacaoRequest);
             return ResponseEntity.status(HttpStatus.CREATED).build();
 
         } catch(IllegalArgumentException exception){
+            log.error("Erro na validação: " + exception);
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
         } catch (Exception e){
+            log.error("Erro na validação: " + e);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
 
     @DeleteMapping
     public ResponseEntity deletar(){
+        log.info("Todas as transacoes foram excluidas");
         transacaoService.deletarTransacoes();
         return ResponseEntity.status(HttpStatus.OK).build();
     }
