fc98b0d1180e4d450cb6190c70e902625bd3dc36
Gabriel Lusa Marostica <gabriellmarostica@gmail.com>
2026-06-26 18:22:11 -0300

    feat: termina validacoes em atributos

--- src/main/java/dev/java10x/itauJava10x/Transacoes/TransacaoRequest.java
+++ src/main/java/dev/java10x/itauJava10x/Transacoes/TransacaoRequest.java
@@ -1,8 +1,11 @@
 package dev.java10x.itauJava10x.Transacoes;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
@@ -12,6 +15,8 @@ import java.time.OffsetDateTime;
 @NoArgsConstructor
 public class TransacaoRequest {
 
+    @NotNull(message = "O valor da transação deve ser igual ou maior a 0")
+    @Positive
     private BigDecimal valor;
     private OffsetDateTime dataHora;
 
